43162c1a4e287daa30726c188bea7e41eefda5b7
ebrahimkrianbd <ebrahim.k.rian.bd@gmail.com>
2018-09-11 10:24:46 +0600

    create client side project

---
+++ PropertyRentalSystem-Client/src/main/java/bd/ac/seu/client/PropertyRentalSystemClientApplication.java
@@ -0,0 +1,12 @@
+package bd.ac.seu.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRentalSystemClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyRentalSystemClientApplication.class, args);
+    }
+}
