8e86a9b12b4142d34aff8ab9b6acb9a0db4be50f
ranasl62 <ranasl62@gmail.com>
2018-09-24 09:12:41 +0600

    'Security'

---
+++ PropertyRentalSystem-Server/src/main/java/bd/ac/seu/server/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package bd.ac.seu.server.configuration;
+
+import bd.ac.seu.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) {
+        try {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+            UserService userService;
+
+            //userService.getUsers().forEach();
+
+            // CW: read the users, passwords and roles from the db
+            // and then load them up in memory
+            builder
+                    .inMemoryAuthentication()
+                    .passwordEncoder(encoder)
+                    .withUser("test")
+                    .password(encoder.encode("rest"))
+                    .roles("admin");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
