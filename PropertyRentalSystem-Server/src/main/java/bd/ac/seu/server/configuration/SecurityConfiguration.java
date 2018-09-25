d45e12b5696b1211dff878d622d1209b6d065f0f
ranasl62 <ranasl62@gmail.com>
2018-09-25 15:02:33 +0600

    Web Security

--- PropertyRentalSystem-Server/src/main/java/bd/ac/seu/server/configuration/SecurityConfiguration.java
+++ PropertyRentalSystem-Server/src/main/java/bd/ac/seu/server/configuration/SecurityConfiguration.java
@@ -1,32 +1,59 @@
-//package bd.ac.seu.server.configuration;
-//
-//import bd.ac.seu.server.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//@EnableWebSecurity
-//public class SecurityConfiguration {
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder builder) {
-//        try {
-//            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//            UserService userService;
-//
-//            //userService.getUsers().forEach();
-//
-//            // CW: read the users, passwords and roles from the db
-//            // and then load them up in memory
-//            builder
-//                    .inMemoryAuthentication()
-//                    .passwordEncoder(encoder)
-//                    .withUser("test")
-//                    .password(encoder.encode("rest"))
-//                    .roles("admin");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package bd.ac.seu.server.configuration;
+
+import bd.ac.seu.server.service.UserService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableJpaRepositories(basePackageClasses = User.class)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+
+    public SecurityConfiguration(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(getPasswordEncoder());
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("**/userName/**").permitAll()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+
+    private PasswordEncoder getPasswordEncoder(){
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return true;
+            }
+        };
+    }
+
+}
